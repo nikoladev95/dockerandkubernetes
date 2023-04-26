de676f287c574f50a8851e968146290fcb6d0ca8
Nikola Milanović <nikola.milanovic.dev@gmail.com>
2023-04-26 12:48:43 +0200

    Visits project changes. Status codes and restart policies

--- visits/index.js
+++ visits/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const redis = require("redis");
+const process = require("process");
 
 const app = express();
 // Connection to the Redis server
@@ -8,9 +9,10 @@ const client = redis.createClient({
     port: 6379
 });
 
-client.set("visits", 0);
+client.set("visits", 1);
 
 app.get("/", (req, res) => {
+    process.exit(0);
     client.get("visits", (err, visits) => {
         res.send("Number of visits is " + visits);
         client.set("visits", parseInt(visits) + 1);
