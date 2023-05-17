52f0f69300589ce99e4a1228bbc76ca7ed107a12
Nikola Milanović <nikola.milanovic.dev@gmail.com>
2023-05-17 17:02:57 +0200

    Multicontainer web application - complete source code

--- complex/worker/index.js
+++ complex/worker/index.js
@@ -2,24 +2,18 @@ const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
 });
-
 const sub = redisClient.duplicate();
 
-// Recursively calculate the Fibonacci value on a certain index
 function fib(index) {
-    if (index < 2) return 1;
-    return fib(index-1) + fib(index - 2);
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
-// Have Redis listen to any value and run the fib() function
-// Every time we get a new message (index), run a callback function
 sub.on('message', (channel, message) => {
-    redisClient.hset('values', message, fib(parseInt(message)));
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
-
-// Return the value to Redis instance
 sub.subscribe('insert');
